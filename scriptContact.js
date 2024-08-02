5a80aa9b8eda89a0475e8736d99b1cb5aa9f3c07
Yaina Ivanova <avonavi24@gmail.com>
2024-08-01 22:43:02 -0600

    caption

--- scriptContact.js
+++ scriptContact.js
@@ -8,7 +8,7 @@
     $form.addEventListener("submit", (e) => {
       e.preventDefault();
       $loader.classList.remove("none");
-      fetch("https://formsubmit.co/ajax/avonavi24@gmail.com", {
+      fetch("https://formsubmit.co/ajax/garciademendoza1@gmail.com", {
         method: "POST",
         body: new FormData(e.target),
       })
